6f306c0b10f4004687b424bdf5f770f1a7d9bfe1
Israel Canessa <israellcanessa@gmail.com>
2020-07-04 07:05:50 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -25,7 +25,7 @@ end
 
 def return_string_array(array)
   counter = 0
+  new_array = []
   while counter < array.length do
-    puts array
-  end
+    array[counter].push(array[counter].to_s)  end
 end
